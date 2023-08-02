0bba030304f333e46ddf1d2394ce4941f491e3ad
saddam andhika <117705270+itsmesadam@users.noreply.github.com>
2023-08-02 09:37:21 +0700

    first commit

---
+++ pages/user/detail.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const BiodataDiri = () => {
+  return (
+    <div>
+        <center>
+      <h1>Biodata Diri</h1>
+      <p>
+<br></br>        
+        <strong>Nama:</strong> Mochammad Saddam Andhika Eka Putra
+      </p>
+<br></br>
+      <p>
+        <strong>Tanggal Lahir:</strong> 11 April 2003
+      </p>
+<br></br>
+      <p>
+        <strong>Tempat Lahir:</strong> Jakarta
+      </p>
+<br></br>
+      <p>
+        <strong>Alamat:</strong> Desa Kalimekar Blok 01, no.10 RT/RW 01/02
+      </p>
+<br></br>      
+      <p>
+        <strong>Pendidikan Terakhir:</strong> SMK 
+      </p>
+<br></br>
+      <p>
+        <strong>Nomor Telepon:</strong> 083195695266
+      </p>
+<br></br>
+      <p>
+        <strong>Alamat Email:</strong> andhikasaddam11@gmail.com
+      </p></center>
+      
+    </div>
+  );
+};
+export default BiodataDiri
